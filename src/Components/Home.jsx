ad7a1e5b01cb75bd749633815ae4b0b0a91a68fa
terenceouma <doros.devs@gmail.com>
2024-08-14 07:23:01 +0300

    creates Home, Login and SignUp pages

---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>This is the Home page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero in
+        exercitationem quasi iste, quidem assumenda quaerat atque enim tenetur,
+        officia consequatur ducimus aperiam minus error recusandae blanditiis,
+        et eos laborum.
+      </p>
+    </>
+  );
+}
+
+export default Home;
